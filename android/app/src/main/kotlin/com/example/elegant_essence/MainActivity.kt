6bd7f23c083ebf5ec3eed87431acc06e780354f8
chathushi24 <cb011177@students.apiit.lk>
2024-09-24 09:02:40 +0530

    Register page

---
+++ android/app/src/main/kotlin/com/example/elegant_essence/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.elegant_essence
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:elegant_essence/signup.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: SignUp.id,
+      routes: {
+        SignUp.id: (context) => SignUp(),
+      },
+    );
+  }
+}
