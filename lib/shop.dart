3d53821018dd7614774508cc086cba2dd882f761
chathushi24 <cb011177@students.apiit.lk>
2024-09-26 22:41:30 +0530

    commit

--- lib/shop.dart
+++ lib/shop.dart
@@ -11,6 +11,16 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Image(
+                image: AssetImage("images/fp1.jpg"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
